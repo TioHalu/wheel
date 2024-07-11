cf9a1f5053599a03d273184c4085d1ed6926d350
tio <agustio@ist.id>
2024-07-12 02:36:56 +0700

    repair

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,13 @@
-"use client"
-import React, { useState } from 'react'
-import { Wheel } from 'react-custom-roulette'
-import { App, ConfigProvider, Button, Modal } from 'antd'
-const data = [
-  { option: '0' },
-  { option: '1' },
-  { option: '2' },
-  { option: '3' },
-  { option: '4' },
-  { option: '5' },
-  { option: '6' },
-]
+import {Suspense} from "react";
+import dynamic from "next/dynamic";
 
-export default () => {
-  const { modal } = App.useApp()
-  const [mustSpin, setMustSpin] = useState(false);
-  const [prizeNumber, setPrizeNumber] = useState(0);
-  const [open, setOpen] = useState(false);
-  console.log(prizeNumber, false)
-  const handleSpinClick = () => {
-    setOpen(false);
-    if (!mustSpin) {
-      const newPrizeNumber = Math.floor(Math.random() * data.length);
-      setPrizeNumber(newPrizeNumber);
-      setMustSpin(true);
-    }
-  }
-
-  const renderModal = () => {
-
-    return (
-      <Modal
-        title="Prize"
-        open={open}
-        onOk={() => {
-          setOpen(false);
-        }}
-        onCancel={() => {
-          setOpen(false);
-        }}
-      >
-        {/* putal audio menang */}
-        <p>Prize number: {prizeNumber}</p>
-      </Modal>
-    )
-  }
-  
+const Component = dynamic(() => import("./pages/component"), {
+  ssr: false,
+})
+export default function Page() {
   return (
-    <ConfigProvider>
-      <App>
-        {renderModal()}
-    <div className='w-full h-screen flex flex-col justify-center items-center'>
-      <Wheel
-        mustStartSpinning={mustSpin}
-        prizeNumber={prizeNumber}
-        data={data}
-
-        onStopSpinning={() => {
-         setOpen(true);
-        }}
-      />
-      <Button onClick={handleSpinClick}>SPIN</Button>
-    </div>
-    </App>
-    </ConfigProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+    <Component />
+    </Suspense>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
-    reactStrictMode: true,
-    rewrites: async () => {
-        return [
-            {
-                source: '/login',
-                destination: '/',
-            },
-        ];
-    }
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
 
 export default nextConfig;
